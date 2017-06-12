cddf74001b7260ea8cfcc95026563fc89b7fd986
Pari <pariksheet@agsft.com>
2017-06-12 10:37:10 +0530

    auth fix

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,7 +8,7 @@ import { AuthService } from '../auth/auth.service';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  constructor(public authService: AuthService) { }
 
   ngOnInit() {
   }
